15c0426c6d3927eb2960375bb6266e673fb9d116
Dragyn85 <nygard.jonas@gmail.com>
2024-12-10 14:55:31 +0100

    level 1 and main menu scene added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,36 +1,76 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] Transform footTransform;
     [SerializeField] LayerMask groundLayer;
-    float speed = 5.0f;
-    float jumpSpeed = 8.0f;
-    float gravity = -20.0f;
+    [SerializeField] LayerMask deadZoneLayer;
+    
+    [SerializeField] float speed = 10.0f;
+    [SerializeField] float jumpSpeed = 2.0f;
+    [SerializeField] float gravity = -20.0f;
+    [SerializeField] float jumpTimerSet = 0.2f;
+
+    float jumpTimer;
+    bool isDead = false;
+    float velocityXSmoothing;
+    Vector2 targetVelocity;
     Rigidbody2D rb;
+    private float fallTimer;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        Physics2D.gravity = new Vector2(0, gravity);
     }
 
     private void Update()
     {
-        if (IsGrounded())
+        if (isDead)
         {
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            move = transform.TransformDirection(move);
-            move *= speed;
-            if (Input.GetButton("Jump"))
-            {
-                move.y = jumpSpeed;
-            }
-
-            rb.velocity = move;
+            return;
         }
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        float targetVelocityX = horizontalInput * speed;
+        float targetVelocityY = 0;
+
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpTimer = jumpTimerSet;
+            
+        }
+        jumpTimer -= Time.deltaTime;
+
+        if (jumpTimer > 0)
+        {
+            targetVelocityY = jumpSpeed;
+        }
+        else
+        {
+            targetVelocityY = rb.velocity.y;
+        }
+
+        targetVelocity = new Vector2(targetVelocityX, targetVelocityY);
+        rb.velocity = targetVelocity;
+        
+        if(IsTouchingDeadZone())
+        {
+            isDead = true;
+            Invoke(nameof(Restart), 2f);
+        }
+    }
+
+    private bool IsTouchingDeadZone()
+    {
+        return Physics2D.Raycast(footTransform.position, Vector2.down, 0.1f, deadZoneLayer);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene("StartMenu");
     }
 
     public bool IsGrounded()
